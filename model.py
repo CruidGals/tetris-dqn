d35e44cea22d1793fb0c7cac48b3297f6aeacef1
CruidGals <kchiem30@gmail.com>
2025-05-15 12:05:47 -0400

    Update model to fit needs

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 class DQNModel(nn.Module):
@@ -6,10 +5,7 @@ class DQNModel(nn.Module):
     def __init__(self, input, output):
         super(DQNModel, self).__init__()
 
-        self.net = nn.Sequential(nn.Conv2d(input, input, kernel_size=5), nn.ReLU(), nn.MaxPool2d(2, 2),
-                                 nn.LazyConv2d(input // 2, kernel_size=3), nn.ReLU(), nn.MaxPool2d(2, 2),
-                                 nn.Flatten(),
-                                 nn.LazyLinear(512), nn.RelU(), nn.Dropout(0.2),
+        self.net = nn.Sequential(nn.Linear(input, 512), nn.RelU(), nn.Dropout(0.2),
                                  nn.Linear(512, 256), nn.ReLU(), nn.Dropout(0.2),
                                  nn.Linear(256, 64), nn.ReLU(), nn.Dropout(0.2),
                                  nn.Linear(64, output))
